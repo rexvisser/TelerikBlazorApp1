f032a7383a19b7d0a7f07a745c409973a11f210b
Rex Visser <rexvisser@gmail.com>
2021-09-24 08:59:19 -0400

    Add a drawer control as the main navigation

---
+++ TelerikBlazorApp1/Shared/MainLayout.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telerik.Blazor.Components;
+
+namespace TelerikBlazorApp1.Shared
+{
+  public partial class MainLayout
+  {
+    private DrawerItem selectedItem;
+
+    private bool DrawerExpanded { get; set; }
+
+    IEnumerable<DrawerItem> Data { get; set; } =
+        new List<DrawerItem>
+        {
+        new DrawerItem {Text = "LIMS", Icon="plus", Url="/lims"},
+        new DrawerItem {Text = "Proto +", Icon="grid-layout", Url="/login"}
+        };
+
+    TelerikDrawer<DrawerItem> DrawerRef { get; set; }
+
+    private DrawerItem SelectedItem
+    {
+      get => selectedItem;
+      set
+      {
+        selectedItem = value;
+      }
+    }
+  }
+
+  /// <summary>
+  /// A menu item in the Drawer.
+  /// </summary>
+  public class DrawerItem
+  {
+    public string Text { get; set; }
+    public string Icon { get; set; }
+    public string Url { get; set; }
+    public bool IsSeparator { get; set; }
+  }
+}
